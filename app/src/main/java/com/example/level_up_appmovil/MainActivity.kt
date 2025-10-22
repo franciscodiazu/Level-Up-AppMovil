baddab75a01b1a7c8c93680bef98540e5d438714
Shaggy_ut <fra.diazu@duocuc.cl>
2025-10-22 11:26:30 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/level_up_appmovil/MainActivity.kt
+++ app/src/main/java/com/example/level_up_appmovil/MainActivity.kt
@@ -9,14 +9,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.level_up_appmovil.navigation.AppNavigation // <-- Importa tu Navegación
+import com.example.level_up_appmovil.navigation.AppNavigation
 import com.example.level_up_appmovil.ui.theme.LevelUpAppMovilTheme
+
+
+
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // Aquí se carga el tema de tu app (definido en ui/theme/Theme.kt)
+            // Aquí se carga el tema del app (definido en ui/theme/Theme.kt)
             LevelUpAppMovilTheme{
 
                 Surface(
